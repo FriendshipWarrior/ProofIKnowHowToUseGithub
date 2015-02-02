08f4694edf552844014e615a2e61351ab6b594ad
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-02-01 20:14:53 -0700

    Initial Commit

---
+++ CS 2450 Assignment 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS 2450 Assignment 4
+//
+//  Created by Dustin Hurst on 2/1/15.
+//  Copyright (c) 2015 Dustin Hurst. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
